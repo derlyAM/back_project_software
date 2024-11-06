1d788988290e0056dac60525acd19539fd7c7562
derlyAM <derly.1702011850@ucaldas.edu.co>
2024-11-06 11:23:22 -0500

    Sin repositorio ni controllers

--- Server/src/controllers/index.ts
+++ Server/src/controllers/index.ts
@@ -1,4 +1,3 @@
 export * from './ping.controller';
 export * from './hello-world.controller';
-export * from './user.controller';
 export * from './hello-world-controller.controller';
--- Server/src/models/index.ts
+++ Server/src/models/index.ts
@@ -1,4 +1,3 @@
-export * from './users.model';
 export * from './review.model';
 export * from './user.model';
 export * from './vehicle.model';
